7b807e83f3513475c01d1a4fff72ec50a3605a77
2112213-FSC-NKU <fscdyx888@163.com>
2024-03-01 20:28:54 +0800

    >  compile 2112213 fsc Linux fsc-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  20:28:54 up 3 min,  1 user,  load average: 0.72, 0.49, 0.21 f71816468295ca15dc084b761750dc0a062e46d

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -13,20 +13,25 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  * cpu.gpr[1]._8[1], we will get the 'ch' register. Hint: Use `union'.
  * For more details about the register encoding scheme, see i386 manual.
  */
+/* Do NOT change the order of the GPRs' definitions. */
 
+/* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
+  * in PA2 able to directly access these registers.
+  */
+ 
+//use union to make diff reg start with same addr
 typedef struct {
-  struct {
-    uint32_t _32;
-    uint16_t _16;
-    uint8_t _8[2];
-  } gpr[8];
-
-  /* Do NOT change the order of the GPRs' definitions. */
-
-  /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
-   * in PA2 able to directly access these registers.
-   */
-  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+  union{
+    union {
+      uint32_t _32;
+      uint16_t _16;
+      uint8_t _8[2];
+    } gpr[8];
+  
+    struct{
+      rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+    };
+  };
 
   vaddr_t eip;
 
