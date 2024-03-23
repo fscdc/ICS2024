de2e1586011fdcd82b1e88781f4a350997ab9d23
2112213-FSC-NKU <fscdyx888@163.com>
2024-03-23 13:13:12 +0800

    >  compile 2112213 fsc Linux fsc-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  13:13:12 up 20 min,  1 user,  load average: 0.28, 0.14, 0.15 4a88c5bb87375139f909e6754cbcfec7703fec7

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -1,8 +1,56 @@
 #include "cpu/exec.h"
 
+//data-mov.c
 make_EHelper(mov);
+make_EHelper(push);
+make_EHelper(pop);
+make_EHelper(pusha);
+make_EHelper(popa);
 
+
+//prefix.c
 make_EHelper(operand_size);
 
+
+//special.c
+make_EHelper(nop);
 make_EHelper(inv);
 make_EHelper(nemu_trap);
+
+
+// logic.c
+make_EHelper(test);
+make_EHelper(and);
+make_EHelper(xor);
+make_EHelper(or);
+make_EHelper(sar);
+make_EHelper(shl);
+make_EHelper(shr);
+make_EHelper(setcc);
+make_EHelper(not);
+
+
+// control.c
+make_EHelper(jmp);
+make_EHelper(jmp_rm);
+make_EHelper(jcc);
+make_EHelper(call);
+make_EHelper(call_rm);
+make_EHelper(ret);
+
+
+// arith.c
+make_EHelper(add);
+make_EHelper(sub);
+make_EHelper(cmp);
+make_EHelper(inc);
+make_EHelper(dec);
+make_EHelper(neg);
+make_EHelper(adc);
+make_EHelper(sbb);
+make_EHelper(mul);
+make_EHelper(imul1);
+make_EHelper(imul2);
+make_EHelper(imul3);
+make_EHelper(div);
+make_EHelper(idiv);
