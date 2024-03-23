e0d172356e5250b8849771016c08e10ba2bfdb93
2112213-FSC-NKU <fscdyx888@163.com>
2024-03-23 21:19:22 +0800

    >  compile 2112213 fsc Linux fsc-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  21:19:22 up  8:26,  1 user,  load average: 0.86, 0.54, 0.51 4a5bf1191d0560bc51ae0ee18d2eabefd4674fc9

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -11,13 +11,21 @@ uint8_t pmem[PMEM_SIZE];
 
 /* Memory accessing interfaces */
 
-uint32_t paddr_read(paddr_t addr, int len) {
-  return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
-}
+// uint32_t paddr_read(paddr_t addr, int len) {
+//   int x = is_mmio(addr);
+//   if(x==-1)
+//     return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+//   else
+//     return mmio_read(addr,len,x);
+// }
 
-void paddr_write(paddr_t addr, int len, uint32_t data) {
-  memcpy(guest_to_host(addr), &data, len);
-}
+// void paddr_write(paddr_t addr, int len, uint32_t data) {
+//   int x = is_mmio(addr);
+//   if(x==-1)
+//     memcpy(guest_to_host(addr), &data, len);
+//   else
+//     mmio_write(addr,len,x);
+// }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
   return paddr_read(addr, len);
