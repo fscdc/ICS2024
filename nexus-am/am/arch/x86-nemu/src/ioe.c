78148dab2232431a8e5feb27abc17b8a29565bec
2112213-FSC-NKU <fscdyx888@163.com>
2024-03-23 21:28:19 +0800

    >  run 2112213 fsc Linux fsc-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  21:28:19 up  8:35,  1 user,  load average: 0.45, 0.71, 0.61 c00419b5f579140d2f18fd6784107fe6c8c2ed8

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -23,9 +23,9 @@ _Screen _screen = {
 extern void* memcpy(void *, const void *, int);
 
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
-  int i;
-  for (i = 0; i < _screen.width * _screen.height; i++) {
-    fb[i] = i;
+  int len = sizeof(uint32_t);
+  for(int i = 0; i < h; i++) {
+      memcpy(fb + (y + i) * _screen.width + x, pixels + i * w, len * w);
   }
 }
 
