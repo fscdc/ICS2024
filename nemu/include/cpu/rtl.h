5edde9e88b464f967e57138934d9a1992496430e
2112213-FSC-NKU <fscdyx888@163.com>
2024-03-23 13:19:24 +0800

    >  compile 2112213 fsc Linux fsc-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  13:19:24 up 26 min,  1 user,  load average: 1.06, 0.52, 0.29 571302b1e0d99bfeb53d914041df80b27b301473

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -136,7 +136,7 @@ static inline void rtl_not(rtlreg_t* dest) {
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  TODO();
+  *dest = ~(*dest);
 }
 
 static inline void rtl_push(const rtlreg_t* src1) {
