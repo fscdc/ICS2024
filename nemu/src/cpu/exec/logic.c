8e9a87bf2b08ac088d56a10a840c7b6f8528b64e
2112213-FSC-NKU <fscdyx888@163.com>
2024-03-23 15:00:03 +0800

    >  compile 2112213 fsc Linux fsc-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  15:00:03 up  2:07,  1 user,  load average: 0.70, 0.49, 0.34 dd32e762ac96ffe9f37ba5306ef7f1f0d3e1072

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -13,8 +13,11 @@ make_EHelper(and) {
 }
 
 make_EHelper(xor) {
-  TODO();
-
+  rtl_xor(&t0, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t0);
+  rtl_update_ZFSF(&t0, id_dest->width);
+  rtl_set_CF(&tzero);
+  rtl_set_OF(&tzero);
   print_asm_template2(xor);
 }
 
