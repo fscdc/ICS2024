e469a96e7eefdbf217a9c6858d13af97dbf2e36b
2112213-FSC-NKU <fscdyx888@163.com>
2024-04-22 00:07:18 +0800

    >  compile 2112213 fsc Linux fsc-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  00:07:18 up  2:48,  1 user,  load average: 0.64, 0.44, 0.31 edf1604e6d6412a095e289f18d8cc9c5221fc373

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,10 +4,16 @@
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
+  uintptr_t res = -1;
 
   switch (a[0]) {
+    case SYS_none: 
+        res = 1;
+        break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+
+  SYSCALL_ARG1(r) = res;
 
   return NULL;
 }
