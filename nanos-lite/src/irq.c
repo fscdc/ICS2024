de31aae0c1c3ed6d5b01371ae6bc74f7e1e1ddf9
2112213-FSC-NKU <fscdyx888@163.com>
2024-05-23 17:28:31 +0800

    >  run 2112213 fsc Linux fsc-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  17:28:31 up  5:45,  1 user,  load average: 0.49, 0.42, 0.39 e9b4f458993d6362396896d7d69f5527bbd8844

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -9,7 +9,6 @@ static _RegSet* do_event(_Event e, _RegSet* r) {
       return do_syscall(r);
     case _EVENT_TRAP:
       printf("Self-Trapped!\n");
-      do_syscall(r);
       return schedule(r);
     default: panic("Unhandled event ID = %d", e.event);
   }
