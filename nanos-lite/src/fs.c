2da0a5c555ed7c9b1b9eff123b507659364e3f3a
2112213-FSC-NKU <fscdyx888@163.com>
2024-06-10 14:42:21 +0800

    >  run 2112213 fsc Linux fsc-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  14:42:21 up 29 min,  1 user,  load average: 0.57, 0.43, 0.33 3ef2a3acb0329649ed1d256fc4a4d6b567bebebf

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -52,7 +52,7 @@ int fs_open(const char* pathname, int flags, int mode) {
 }
 
 int fs_close(int fd){
-  // file_table[fd].open_offset = 0;
+  file_table[fd].open_offset = 0;
   Log("After close \n");
   return 0;
 }
